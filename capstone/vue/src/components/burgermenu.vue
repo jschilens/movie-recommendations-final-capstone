8aa1b89998ac0156d46b18ac545b576567750e0a
Alain <a.natan.delgado@outlook.com>
2022-09-28 17:33:20 -0400

    Burger menu progress

--- capstone/vue/src/components/burgermenu.vue
+++ capstone/vue/src/components/burgermenu.vue
@@ -1,22 +1,21 @@
 <template>
   <div id="sidemenu">
-      <button class="sidemenu-button" v-on:click="showNav = !showNav" v-bind:class="{active:showNav}">
-      <span class="top">1</span>
-      <span class="mid">2</span>
-      <span class="bottom">3</span>
+      <button class="sidemenu-button" v-on:click="showNav = !showNav" >
+      <div class="first-line"></div>
+      <div class="sec-line"></div>
+      <div class="third-line"></div>
       </button>
       <transition>
       <nav v-show="showNav">
-        <div class="sidemenu__wrapper">
-          <ul class="sidemenu__list">
-            <li class="sidemenu__item"><a href="">test</a></li>
-            <li class="sidemenu__item"><a href="">test</a></li>
-            <li class="sidemenu__item"><a href="">test</a></li>
+        <div class="sidemenu-box">
+          <ul class="sidemenu-list">
+            <router-link id="list" v-bind:to="{ name: 'home' }" >Home</router-link>
+            <router-link id="list" v-bind:to="{ name: 'movies' }" >Browse</router-link>
+            <router-link id="list" v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''" >Logout</router-link>
           </ul>
         </div>
       </nav>
     </transition>
-
 
 
   </div>
@@ -36,6 +35,43 @@ export default {
 
 <style>
 
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
 
+
+nav{
+    /* box */
+    width: 300px;
+    height: 100px;
+    background: white;
+}
+
+.sidemenu-button {
+    background-color: #484848;
+    border: 0;
+}
+
+button div {
+    /* button display */
+    width: 35px;
+    height: 5px;
+    background-color: white;
+    margin: 6px 0;
+}
+
+#list{
+    display: flex;
+    flex-direction: column;
+}
+
+#list{
+    text-decoration: none;
+    text-align: center;
+    font-size: 25px;
+    
+}
 
 </style>
